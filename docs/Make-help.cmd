a98de6974d53b31601e64763453c2c79a1d0ecad
Jürgen Rathlev <40795066+jrathlev@users.noreply.github.com>
2018-08-04 17:14:44 +0200

    Add files via upload

---
+++ docs/Make-help.cmd
@@ -0,0 +1,7 @@
+@rem compile Personal Backup help files
+
+"%ProgramFiles(x86)%\HTML Help Workshop\hhc.exe" en\DosPanel.hhp
+"%ProgramFiles(x86)%\HTML Help Workshop\hhc.exe" de\DosPanel.hhp
+
+pause
+
